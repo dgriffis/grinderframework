b7223633dbaf9110056f8a0568718baacb7b7817
Dan Griffis <dgriffis@glgroup.com>
2012-12-20 16:16:40 -0500

    Add scenario support and jtl output to logCompare

--- logCompare.py
+++ logCompare.py
@@ -5,6 +5,7 @@ import sys
 
 import unittest
 from xml.dom.minidom import parseString
+from testscenarios import TestWithScenarios
 import xmlrunner
 
 #The only change should be the Qtime - any other change should be investigated
@@ -12,8 +13,8 @@ rspByteWarnAt = 1
 
 def get_args():
     parser = optparse.OptionParser()
-    parser.add_option("-b", "--beforeLog", action="store", type="string", dest="beforeLog", default="before-0-data.log",help="name of grinder baseline run data.log file.")
-    parser.add_option("-c", "--afterLog", action="store", type="string", dest="afterLog", default="after-0-data.log", help="name of grinder change run data.log file.")
+    parser.add_option("-b", "--beforeLog", action="store", type="string", dest="beforeLog", default="devNightly-0-data.log",help="name of grinder baseline run data.log file.")
+    parser.add_option("-c", "--afterLog", action="store", type="string", dest="afterLog", default="devDeploy-0-data.log", help="name of grinder change run data.log file.")
     parser.add_option("-l", "--logDir", action="store", type="string", dest="logDir", default="/Users/dgriffis/grinder/MyTests/Search_xmlrpc/log/", help="directory containing grinder data.log files.")
     
     return parser
@@ -61,59 +62,64 @@ def runCompare(baselineAr,changeAr):
     for blLine, chLine in zip(baselineAr, changeAr): 
         compareRow(blLine, chLine)
 
+def _buildScenarios( numoftests):
+    #scenario =  [('Row1', dict(param='1')),
+    #             ('Row2', dict(param='2'))
+    #              ]
+    scenario = []   
+    for x in range(0, numoftests):
+        scenarioName = "scenario_"+str(x)
+        scenario.append( (scenarioName, dict(param=x) ))
+   
+    return scenario
 
-class Test(unittest.TestCase):
+class Test(TestWithScenarios):
     changeAr = []
     baselineAr = []
+    parser = get_args()
+    (options, args) = parser.parse_args()
+    #print "arg values are %s" % args
+    #print "options are %s" % options
     
+    logDir = options.logDir
+    #print "logdir is %s" % logDir 
+    beforeLog = options.beforeLog
+    #print "beforeLog is %s" % beforeLog 
+    afterLog = options.afterLog
+    #print "afterLog is %s" % afterLog
+    baselineAr = buildArray(logDir, beforeLog)
+    changeAr = buildArray(logDir, afterLog)
+  
+    scenarios = _buildScenarios( len(baselineAr) ) 
+    #print scenarios
+            
     def setUp(self):
-        parser = get_args()
-        (options, args) = parser.parse_args()
-        #print "arg values are %s" % args
-        #print "options are %s" % options
-    
-        self.logDir = options.logDir
-        print "logdir is %s" % self.logDir 
-        self.beforeLog = options.beforeLog
-        print "beforeLog is %s" % self.beforeLog 
-        self.afterLog = options.afterLog
-        print "afterLog is %s" % self.afterLog 
-        self.baselineAr = buildArray(self.logDir, self.beforeLog)
-        self.changeAr = buildArray(self.logDir, self.afterLog)
-    
+        self.assertTrue( len(self.baselineAr) == len(self.changeAr) )
+   
     def tearDown(self):
         pass
 
-    def testLogFileRanSameNumberofTests(self):
-        self.assertTrue( len(self.baselineAr) == len(self.changeAr) )
-
     def testLogFileTestRunTimesComparable(self):
-        numoftestfails = 0
-        for blLine, chLine in zip(self.baselineAr, self.changeAr):
-            blData = blLine.split(",")
-            chData = chLine.split(",")
-            testNum = blData[LogHeaders.Test]
-            if testNum.strip() == "Test":          
-                continue
-            blVal = int(blData[LogHeaders.Test_time])
-            chVal = int(chData[LogHeaders.Test_time])
-            if blVal == 0 or chVal == 0:
-                continue
+        blLine = self.baselineAr[self.param]
+        chLine = self.changeAr[self.param]
+        blData = blLine.split(",")
+        chData = chLine.split(",")
+        testNum = blData[LogHeaders.Test]
+        if testNum.strip() == "Test":          
+            return
+        blVal = int(blData[LogHeaders.Test_time])
+        chVal = int(chData[LogHeaders.Test_time])
+        if blVal == 0 and chVal == 0:
+            return
             
-            pctDiff = 100 - ( float(chVal) / float(blVal) * 100 ) if blVal > chVal else 100 - ( float(blVal) / float(chVal) * 100 )
+        pctDiff = 100 - ( float(chVal) / float(blVal) * 100 ) if blVal > chVal else 100 - ( float(blVal) / float(chVal) * 100 )
 
-            try:
-                self.assertLessEqual( pctDiff , 50 )
-                
-            except:
-                numoftestfails+=1
-                print "Failed Tests is %d with a pctDiff of %d - this test run is %s with a baseline of %d and comparison of %d"  % (numoftestfails, pctDiff, blData[LogHeaders.Run], blVal, chVal)
-                continue
-                
+        self.assertLessEqual( pctDiff , 50 )
+                                
 if __name__ == "__main__":
-    unittest.main( testRunner=xmlrunner.XMLTestRunner(output='/Users/Shared/Jenkins/Home/jobs/Search_xmlrpc/workspace/test-reports') )
-    
+
+    jtlFolder = "/Users/Shared/Jenkins/Home/jobs/Search_xmlrpc/workspace/test-reports"
     suite = unittest.TestLoader().loadTestsFromTestCase(Test)
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    
+    #unittest.TextTestRunner(verbosity=2).run(suite)
+    xmlrunner.XMLTestRunner(output=jtlFolder).run(suite)
     
